acd56efb4879ff78ccc295219f21460a6a1c7cbe
Siikana <jonas.lemstrom@gmail.com>
2023-05-02 11:43:36 +0300

    2.5.

--- java.js
+++ java.js
@@ -43,17 +43,17 @@ function tarkista_paiva(limit) {
 
 // eka kuva
 function change_image1() { 
-  let paivamaaran_tarkistus = tarkista_paiva("5.22.2023"); // pitää olla kk.pv.vuosi  --- tästä voi testata että toimii, jos vaikka laittaa 4.22.2023 niin silloin luukku aukeaa :D
+  let paivamaaran_tarkistus = tarkista_paiva("5.1.2023"); // pitää olla kk.pv.vuosi  --- tästä voi testata että toimii, jos vaikka laittaa 4.22.2023 niin silloin luukku aukeaa :D
 
   if (paivamaaran_tarkistus == true) {
-  if (document.getElementById("luukku1").src == "Kuvat/question_mark.png"){
-      document.getElementById("luukku1").src = "Kuvat/kuva.jpg"; 
-      document.getElementById("card-title").innerHTML = "New Card Title"; // tällä sai vaihdettuu myös ton tekstin 
+  if (document.getElementById("luukku1").src == "Kuvat/kansi.jpg"){
+      document.getElementById("luukku1").src = "kuvat/suokki.png"; 
+      document.getElementById("card-title").innerHTML = "Suomenlinna"; // tällä sai vaihdettuu myös ton tekstin 
   document.getElementById("card-text").innerHTML = "New Card Text";
       
   } else {
-      document.getElementById("luukku1").src = "Kuvat/kuva.jpg";
-      document.getElementById("card-title").innerHTML = "New Card Title"; 
+      document.getElementById("luukku1").src = "Kuvat/suokki.png";
+      document.getElementById("card-title").innerHTML = "Suomenlinna"; 
   document.getElementById("card-text").innerHTML = "New Card Text";
   }
 }
@@ -62,17 +62,17 @@ function change_image1() {
 // toka kuva
 
 function change_image2() {
-  let paivamaaran_tarkistus = tarkista_paiva("5.29.2023"); // pitää olla kk.pv.vuosi
+  let paivamaaran_tarkistus = tarkista_paiva("5.2.2023"); // pitää olla kk.pv.vuosi
 
   if (paivamaaran_tarkistus == true) {
-  if (document.getElementById("luukku2").src == "Kuvat/question_mark.png"){
-      document.getElementById("luukku2").src = "Kuvat/kuva.jpg";
-      document.getElementById("card-title-2").innerHTML = "New Card Title"; // tällä sai vaihdettuu myös ton tekstin <3
+  if (document.getElementById("luukku2").src == "Kuvat/kansi.jpg"){
+      document.getElementById("luukku2").src = "Kuvat/allas.png";
+      document.getElementById("card-title-2").innerHTML = "Allas sea pool"; // tällä sai vaihdettuu myös ton tekstin <3
   document.getElementById("card-text-2").innerHTML = "New Card Text";
       
   } else {
-      document.getElementById("luukku2").src = "Kuvat/kuva.jpg";
-      document.getElementById("card-title-2").innerHTML = "New Card Title"; 
+      document.getElementById("luukku2").src = "Kuvat/allas.png";
+      document.getElementById("card-title-2").innerHTML = "Allas sea pool"; 
   document.getElementById("card-text-2").innerHTML = "New Card Text";
   }
 }
@@ -80,10 +80,10 @@ function change_image2() {
 
 // kolmas kuva
 function change_image3() {
-  let paivamaaran_tarkistus = tarkista_paiva("6.5.2023"); // pitää olla kk.pv.vuosi
+  let paivamaaran_tarkistus = tarkista_paiva("5.2.2023"); // pitää olla kk.pv.vuosi
 
   if (paivamaaran_tarkistus == true) {
-  if (document.getElementById("luukku3").src == "Kuvat/question_mark.png"){
+  if (document.getElementById("luukku3").src == "Kuvat/kansi.jpg"){
       document.getElementById("luukku3").src = "Kuvat/kuva.jpg";
       document.getElementById("card-title-3").innerHTML = "New Card Title"; // tähä muokatut tekstit <3
   document.getElementById("card-text-3").innerHTML = "New Card Text";
@@ -99,10 +99,10 @@ function change_image3() {
 //neljäs kuva
 
 function change_image4() {
-  let paivamaaran_tarkistus = tarkista_paiva("6.12.2023"); // pitää olla kk.pv.vuosi
+  let paivamaaran_tarkistus = tarkista_paiva("5.2.2023"); // pitää olla kk.pv.vuosi
 
   if (paivamaaran_tarkistus == true) {
-  if (document.getElementById("luukku4").src == "Kuvat/question_mark.png"){
+  if (document.getElementById("luukku4").src == "Kuvat/kansi.jpg"){
       document.getElementById("luukku4").src = "Kuvat/kuva.jpg";
       document.getElementById("card-title-4").innerHTML = "New Card Title"; // tähä muokatut tekstit <3
   document.getElementById("card-text-4").innerHTML = "New Card Text"; //tähä muokatut tekstit <3
@@ -117,10 +117,10 @@ function change_image4() {
 
 // viides kuva
 function change_image5() {
-  let paivamaaran_tarkistus = tarkista_paiva("6.19.2023"); // pitää olla kk.pv.vuosi
+  let paivamaaran_tarkistus = tarkista_paiva("5.2.2023"); // pitää olla kk.pv.vuosi
 
   if (paivamaaran_tarkistus == true) {
-  if (document.getElementById("luukku5").src == "Kuvat/question_mark.png"){
+  if (document.getElementById("luukku5").src == "Kuvat/kansi.jpg"){
       document.getElementById("luukku5").src = "Kuvat/kuva.jpg";
       document.getElementById("card-title-5").innerHTML = "New Card Title"; // tähä muokatut tekstit <3
   document.getElementById("card-text-5").innerHTML = "New Card Text"; //tähä muokatut tekstit <3
@@ -135,10 +135,10 @@ function change_image5() {
 
 // kuudes kuva
 function change_image6() {
-  let paivamaaran_tarkistus = tarkista_paiva("6.26.2023"); // pitää olla kk.pv.vuosi
+  let paivamaaran_tarkistus = tarkista_paiva("5.2.2023"); // pitää olla kk.pv.vuosi
 
   if (paivamaaran_tarkistus == true) {
-  if (document.getElementById("luukku6").src == "Kuvat/question_mark.png"){
+  if (document.getElementById("luukku6").src == "Kuvat/kansi.jpg"){
       document.getElementById("luukku6").src = "Kuvat/kuva.jpg";
       document.getElementById("card-title-6").innerHTML = "New Card Title"; // tähä muokatut tekstit <3
   document.getElementById("card-text-5").innerHTML = "New Card Text"; //tähä muokatut tekstit <3
@@ -154,10 +154,10 @@ function change_image6() {
 // seitsemäs kuva
 
 function change_image7() {
-  let paivamaaran_tarkistus = tarkista_paiva("7.3.2023"); // pitää olla kk.pv.vuosi
+  let paivamaaran_tarkistus = tarkista_paiva("2.3.2023"); // pitää olla kk.pv.vuosi
 
   if (paivamaaran_tarkistus == true) {
-  if (document.getElementById("luukku7").src == "Kuvat/question_mark.png"){
+  if (document.getElementById("luukku7").src == "Kuvat/kansi.jpg"){
       document.getElementById("luukku7").src = "Kuvat/kuva.jpg";
       document.getElementById("card-title-7").innerHTML = "New Card Title"; // tähä muokatut tekstit <3
   document.getElementById("card-text-7").innerHTML = "New Card Text"; //tähä muokatut tekstit <3
@@ -173,10 +173,10 @@ function change_image7() {
 // kahdeksas kuva
 
 function change_image8() {
-  let paivamaaran_tarkistus = tarkista_paiva("7.10.2023"); // pitää olla kk.pv.vuosi
+  let paivamaaran_tarkistus = tarkista_paiva("2.10.2023"); // pitää olla kk.pv.vuosi
 
   if (paivamaaran_tarkistus == true) {
-  if (document.getElementById("luukku8").src == "Kuvat/question_mark.png"){
+  if (document.getElementById("luukku8").src == "Kuvat/kansi.jpg"){
       document.getElementById("luukku8").src = "Kuvat/kuva.jpg";
       document.getElementById("card-title-8").innerHTML = "New Card Title"; // tähä muokatut tekstit <3
   document.getElementById("card-text-8").innerHTML = "New Card Text"; //tähä muokatut tekstit <3
@@ -192,10 +192,10 @@ function change_image8() {
 //yhdeksäs 
 
 function change_image9() {
-  let paivamaaran_tarkistus = tarkista_paiva("7.17.2023"); // pitää olla kk.pv.vuosi
+  let paivamaaran_tarkistus = tarkista_paiva("2.17.2023"); // pitää olla kk.pv.vuosi
 
   if (paivamaaran_tarkistus == true) {
-  if (document.getElementById("luukku9").src == "Kuvat/question_mark.png"){
+  if (document.getElementById("luukku9").src == "Kuvat/kansi.jpg"){
       document.getElementById("luukku9").src = "Kuvat/kuva.jpg";
       document.getElementById("card-title-9").innerHTML = "New Card Title"; // tähä muokatut tekstit <3
   document.getElementById("card-text-9").innerHTML = "New Card Text"; //tähä muokatut tekstit <3
@@ -211,10 +211,10 @@ function change_image9() {
 //kymmenes
 
 function change_image10() {
-  let paivamaaran_tarkistus = tarkista_paiva("7.24.2023"); // pitää olla kk.pv.vuosi
+  let paivamaaran_tarkistus = tarkista_paiva("2.24.2023"); // pitää olla kk.pv.vuosi
 
   if (paivamaaran_tarkistus == true) {
-  if (document.getElementById("luukku10").src == "Kuvat/question_mark.png"){
+  if (document.getElementById("luukku10").src == "Kuvat/kansi.jpg"){
       document.getElementById("luukku10").src = "Kuvat/kuva.jpg";
       document.getElementById("card-title-10").innerHTML = "New Card Title"; // tähä muokatut tekstit <3
   document.getElementById("card-text-10").innerHTML = "New Card Text"; //tähä muokatut tekstit <3
@@ -230,10 +230,10 @@ function change_image10() {
 // 11.kuva
 
 function change_image11() {
-  let paivamaaran_tarkistus = tarkista_paiva("7.31.2023"); // pitää olla kk.pv.vuosi
+  let paivamaaran_tarkistus = tarkista_paiva("2.31.2023"); // pitää olla kk.pv.vuosi
 
   if (paivamaaran_tarkistus == true) {
-  if (document.getElementById("luukku11").src == "Kuvat/question_mark.png"){
+  if (document.getElementById("luukku11").src == "Kuvat/kansi.jpg"){
       document.getElementById("luukku11").src = "Kuvat/kuva.jpg";
       document.getElementById("card-title-11").innerHTML = "New Card Title"; // tähä muokatut tekstit <3
   document.getElementById("card-text-11").innerHTML = "New Card Text"; //tähä muokatut tekstit <3
@@ -249,10 +249,10 @@ function change_image11() {
 // 12.kuva
 
 function change_image12() {
-  let paivamaaran_tarkistus = tarkista_paiva("8.7.2023"); // pitää olla kk.pv.vuosi
+  let paivamaaran_tarkistus = tarkista_paiva("2.7.2023"); // pitää olla kk.pv.vuosi
 
   if (paivamaaran_tarkistus == true) {
-  if (document.getElementById("luukku12").src == "Kuvat/question_mark.png"){
+  if (document.getElementById("luukku12").src == "Kuvat/kansi.jpg"){
       document.getElementById("luukku12").src = "Kuvat/kuva.jpg";
       document.getElementById("card-title-12").innerHTML = "New Card Title"; // tähä muokatut tekstit <3
   document.getElementById("card-text-12").innerHTML = "New Card Text"; //tähä muokatut tekstit <3
@@ -268,10 +268,10 @@ function change_image12() {
 //13.kuva
 
 function change_image13() {
-  let paivamaaran_tarkistus = tarkista_paiva("8.14.2023"); // pitää olla kk.pv.vuosi
+  let paivamaaran_tarkistus = tarkista_paiva("2.14.2023"); // pitää olla kk.pv.vuosi
 
   if (paivamaaran_tarkistus == true) {
-  if (document.getElementById("luukku13").src == "Kuvat/question_mark.png"){
+  if (document.getElementById("luukku13").src == "Kuvat/kansi.jpg"){
       document.getElementById("luukku13").src = "Kuvat/kuva.jpg";
       document.getElementById("card-title-13").innerHTML = "New Card Title"; // tähä muokatut tekstit <3
   document.getElementById("card-text-13").innerHTML = "New Card Text"; //tähä muokatut tekstit <3
